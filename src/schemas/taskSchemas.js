3f533509436247155898f80a681ebbbac5a12c94
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-01 17:54:43 +0200

    feat(schemas): Add manualTimeSchema and tags

--- src/schemas/taskSchemas.js
+++ src/schemas/taskSchemas.js
@@ -4,7 +4,8 @@ export const createTaskSchema = z.object({
     body: z.object({
         name: z.string({ required_error: 'Name is required'})
             .min(3, 'Name must be at least 3 characters long')
-            .max(100, 'Name is too long')
+            .max(100, 'Name is too long'),
+        tags: z.array(z.string()).optional().default([])
     })
 });
 
@@ -15,5 +16,19 @@ export const addSubtaskSchema = z.object({
     body: z.object({
         name: z.string({ required_error: 'Subtask name is required' })
         .min(3, 'Must be at least 3 characters long')
+    })
+});
+
+export const manualTimeSchema = z.object({
+    params: z.object({
+        id: z.uuid(),
+        subtaskId: z.uuid().optional()
+    }),
+    body: z.object({
+        start: z.iso.datetime({error: "Invalid start date"}),
+        end: z.iso.datetime({error: "Invalid end date"}),
+    }).refine(data => new Date(data.start) < new Date(data.end), {
+        error: "The start date should be before the end date",
+        path: ["start"]
     })
 });
